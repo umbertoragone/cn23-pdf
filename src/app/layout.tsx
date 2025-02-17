613fdcce8e67fabb1de49cee348b63d2c8389525
Umberto Ragone <umberto@ragone.dev>
2025-02-17 12:21:38 +0100

    Update component styles and configurations; refactor UI components for improved structure and accessibility

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased md:overflow-hidden dark:dark`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased md:overflow-hidden`}
       >
         {children}
       </body>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "tailwindcss";
 import forms from "@tailwindcss/forms";
 
 export default {
+  darkMode: "media",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
